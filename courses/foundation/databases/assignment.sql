30949b3792d61e1289a43856b0b22c700f9b18f2
Ahmadreza Pakraei <arpakraei@gmail.com>
2025-12-16 20:49:30 +0100

    feat: add SQL queries for task and status analysis

---
+++ courses/foundation/databases/assignment.sql
@@ -0,0 +1,54 @@
+
+/*How many tasks are in the task table? */
+select count(*) as [Number Of Task] from task;
+
+/*How many tasks in the task table do not have a valid due date?*/
+select count(*) as [Invalid_Due_Date]
+from task 
+where due_date is NULL ;
+
+/*Find all the tasks that are marked as done.*/
+select * 
+from task 
+where status_id =3;
+
+/*Find all the tasks that are not marked as done.*/
+select * 
+from task 
+where (status_id  in (1,2));
+
+/*Get all the tasks, sorted with the most recently created first.*/
+select * 
+from task 
+order by created DESC ;
+
+/*Get the single most recently created task.*/
+select * 
+from task 
+order by created DESC 
+limit 1;
+
+/*Get the title and due date of all tasks where the title or description contains database.*/
+select title, due_date 
+from task 
+where (title like '%database%')or(description  like '%database%');
+
+/*Get the title and status (as text) of all tasks.*/
+select task.title , status.name 
+from task , status 
+where task.status_id =status.id ;
+
+/*Get the name of each status, along with a count of how many tasks have that status.*/
+select status.name  , count(*) as [Number of Task]
+from task,status 
+where task.status_id = status.id 
+GROUP by status_id ;
+
+/*Get the names of all statuses, sorted by the status with most tasks first.*/
+select status.name  , count(*) as Number_of_Task 
+from task,status 
+where task.status_id = status.id 
+GROUP by status_id 
+ORDER by Number_of_Task DESC 
+Limit 1;
+
